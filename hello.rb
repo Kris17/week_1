74f413ec9bab5076a3f8f1d8cf0abd35fa71c697
kristina.petrova <kristina.petrova@iaau.edu.kg>
2015-02-17 21:05:11 +0600

    commit

---
+++ hello.rb
@@ -0,0 +1,28 @@
+def greet(name)
+=begin
+	(string) -> string
+	
+	this method has to accept string
+	and return string
+
+	>  greet("Azamat")
+	=> "Hello, Azamat!"
+=end
+	return 'Hello, ' + name + '!'
+end
+
+
+def hello
+=begin =>
+	() -> string
+
+	this method does not have to accept
+	any arguments and it has to return
+	string
+
+	>  hello()
+	=> "Hello!"
+=end
+  return 'Hello!'
+end
+
